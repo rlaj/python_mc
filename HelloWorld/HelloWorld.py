cfff0df8e27501deb9f6ef0593c64f8d3b170d26
rlaj <rlajaunie@hotmail.com>
2019-02-01 04:37:48 -0600

    hello world and variables

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,16 @@
+# greeting = "Hello"
+# name = input("Please enter your name ")
+# print(greeting + ' ' + name)
+#
+# splitString = "This string has been\nsplit over\nseveral\nline"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4"
+# print(tabbedString)
+
+print('The pet shop owner said "No, no, \'he\'s uh,...he\s resting"')
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+print(anotherSplitString)
---
+++ Variables/variables.py
@@ -0,0 +1,39 @@
+# a = 12
+# b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+#
+#
+# for i in range(1, a//b):
+#     print(i)
+
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[3])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[6:])
+print(parrot[:6])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+
+number = "9,223,372,036,054,775,807"
+print(number[1::4])
+number = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(number[0::3])
+string1 = "he's "
+string2 = "probably "
+print(string1 + string2)
+print("he's " "probably " "pining")
+print("Hello " * 5)
+print("Hello " * (5 + 4))
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
+print("parrot" in "fjord")
